95cf517505ba6e288be9340ef28dea45a27096d2
germanespinosa <germanespinosa@gmail.com>
2020-08-05 08:20:08 -0500

    first version

--- include/predator.h
+++ include/predator.h
@@ -2,6 +2,7 @@
 #include <cell_world.h>
 
 struct Predator_state : cell_world::Agent_internal_state {
+    cell_world::Cell goal;
 };
 
 struct Predator : cell_world::Stateful_agent<Predator_state> {
@@ -10,9 +11,11 @@ struct Predator : cell_world::Stateful_agent<Predator_state> {
     const cell_world::Cell &start_episode() override;
     cell_world::Move get_move(const cell_world::Model_public_state &) override;
     cell_world::Agent_status_code update_state(const cell_world::Model_public_state &) override;
-    void end_episode(const cell_world::Model_public_state &) override;
     const cell_world::Graph &world_graph;
     const cell_world::Graph &visibility;
+    const cell_world::Graph inverted_visibility;
     const cell_world::Paths &paths;
     cell_world::Cell start_cell;
+private:
+    void _process_state(const cell_world::Model_public_state &);
 };
--- src/predator.cpp
+++ src/predator.cpp
@@ -11,6 +11,7 @@ Predator::Predator(
         const cell_world::Cell &start_cell):
         world_graph(world_graph),
         visibility(visibility),
+        inverted_visibility(visibility.invert()),
         paths(paths),
         start_cell(start_cell){}
 
@@ -20,21 +21,34 @@ Predator::Predator(
         const cell_world::Paths &paths):
         world_graph(world_graph),
         visibility(visibility),
+        inverted_visibility(visibility.invert()),
         paths(paths){}
 
 const cell_world::Cell &Predator::start_episode() {
+    internal_state().goal = start_cell;
     return start_cell;
 }
 
-cell_world::Move Predator::get_move(const cell_world::Model_public_state &) {
-    return cell_world::Move();
+cell_world::Move Predator::get_move(const cell_world::Model_public_state &state) {
+    _process_state(state);
+    auto &prey_cell = state.agents_state[0].cell;
+    auto &predator_cell = state.agents_state[1].cell;
+    return paths.get_move(prey_cell, predator_cell);
 }
 
-cell_world::Agent_status_code Predator::update_state(const cell_world::Model_public_state &) {
-    return cell_world::Finished;
+cell_world::Agent_status_code Predator::update_state(const cell_world::Model_public_state &state) {
+    _process_state(state);
+    return cell_world::Running;
 }
 
-void Predator::end_episode(const cell_world::Model_public_state &) {
-
+void Predator::_process_state(const cell_world::Model_public_state &state) {
+    auto &prey_cell = state.agents_state[0].cell;
+    auto &predator_cell = state.agents_state[1].cell;
+    if (visibility[predator_cell].contains(prey_cell)) {
+        internal_state().goal = prey_cell;
+        return;
+    }
+    if (predator_cell == internal_state().goal) {
+        internal_state().goal = inverted_visibility[predator_cell].random_cell();
+    }
 }
-

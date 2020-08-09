20d0da48b27c17940a76b3c92a534a46a05b4234
germanespinosa <germanespinosa@gmail.com>
2020-08-09 18:33:26 -0500

    buildable

--- src/reward.cpp
+++ src/reward.cpp
@@ -15,6 +15,6 @@ double Reward::compute(Result result, unsigned int steps) const {
             break;
     }
     if ( step ) base_reward += step  * (double) steps;
-    if ( discount != 1 ) base_reward *= pow(discount, steps);
+    if ( discount ) base_reward *= pow((1 - discount), steps);
     return base_reward;
 }

05f2df591f015f2aca71dbf2d334588a8a5d9fb0
German <germanespinosa@gmail.com>
2024-11-15 11:47:41 -0600

    barely working

--- src/knowledge_run.cpp
+++ src/knowledge_run.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     params_cpp::Parser p(argc, argv);
     Knowledge_simulation_parameters parameters;
-    parameters.
+    parameters.goal = cell_world::Coordinates(20,0);
+    parameters.knowledge_file = "knowledge.dat";
+    parameters.particle_filter.attempts = 1000;
+    parameters.particle_filter.particle_count = 100;
+    parameters.path_type = "astar";
+    parameters.result_file = "result.dat";
+    parameters.seeds = {0, 1,2,3,4,5,6,7,8,9};
+    parameters.steps = 200;
+    parameters.world_info.world_configuration = "hexagonal";
+    parameters.world_info.world_implementation = "canonical";
+    parameters.world_info.occlusions = "21_05";
     Knowledge_simulation simulation(parameters);
     simulation.run();
 }
--- src/run.cpp
+++ src/run.cpp
@@ -5,7 +5,23 @@ using namespace std;
 int main(int argc, char **argv)
 {
     Simulation_parameters p;
-    p.load(argc, argv);
+    p.particle_filter.attempts = 1000;
+    p.particle_filter.particle_count = 100;
+    p.path_type = "astar";
+    p.planner.reward.discount = .1;
+    p.planner.reward.fail = -100;
+    p.planner.roll_outs = 100;
+    p.planner.use_pois = true;
+    p.planner.tree_mode = "mcts";
+    p.predator.randomness = .25;
+    p.predator.speed = 1.5;
+    p.predator_location = cell_world::Coordinates(10,10);
+    p.predator_present = true;
+    p.prey.goal = cell_world::Coordinates(20,0);
+    p.seeds = {0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    p.world_info.world_configuration = "hexagonal";
+    p.world_info.world_implementation = "canonical";
+    p.world_info.occlusions = "21_05";
     Simulation simulation(p);
     simulation.run();
 }
